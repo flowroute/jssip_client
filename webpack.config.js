9169ac910ad79b7bc03d2e90bb9634fe54a516e6
mazuh <marcell-mz@hotmail.com>
2019-08-02 19:55:08 -0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,18 @@ module.exports = {
     path: path.resolve(__dirname, 'demo'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
